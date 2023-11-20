28d69fa1b49f543613ef9a72ddeec096e62ff1c0
Christopher Wells <139442127+christopher-wells@users.noreply.github.com>
2023-11-20 18:45:38 +0000

    Added some maximum bet logic.

--- casino.py
+++ casino.py
@@ -1,6 +1,6 @@
 class Hand:
     """
-    A Hand consists of a number of Cards. The cards can have a total.
+    A Hand consists of a number of Cards. The Cards can have a total.
     """
 
     def __init__(self) -> None:
@@ -16,28 +16,40 @@ class Hand:
 
 class Box:
     """
-    A box is a position in a Game that contains wagers. In most games the
-    maximum amount of wagers will be limited per box.
+    A box is a position in a Game that contains Wagers. In most games the
+    maximum amount of Wagers will be limited per box.
+    There can be a maximum bet amount that limits the amount placed in a wager.
     """
 
-    def __init__(self, max_wagers=0) -> None:
+    def __init__(self, max_wagers=0, max_bet=0) -> None:
         self.max_wagers = max_wagers
+        self.max_bet = max_bet
         self.wagers = []
         self.active = False
         self.action = False
         self.hand = None
+        self.invalid_bet = False
+    
+    def calculate_if_max_bet_exceeded(self) -> bool:
+        box_total = 0
+        for wager in self.wagers:
+            box_total += wager.total
+        if box_total > self.max_bet:
+            self.invalid_bet = True
+            return self.invalid_bet
+        else:
+            self.invalid_bet = False
+            return self.invalid_bet        
 
 
 class Wager:
     """
-    A wager is an amount that will be taken from the Players balance and used in
-    a Box to play a Game. There can be a maximum bet amount that limits the
-    amount placed in a wager.
+    A Wager is an amount that will be taken from the Players balance and used in
+    a Box to play a Game.
     """
 
     def __init__(self) -> None:
         self.total = 0
-        self.max_bet = 500
 
 
 class Player:
@@ -48,8 +60,7 @@ class Player:
         self.previous_wager = 0
     
     def __str__(self) -> str:
-        return f"{self.name} has £{self.balance} left."
-        
+        return f"{self.name} has £{self.balance} left."  
 
 
 class Dealer:
